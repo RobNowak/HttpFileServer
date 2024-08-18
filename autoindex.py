73cd02e49cc49212931ad31beb189feafe89059b
RobNowak <RobNowak@users.noreply.github.com>
2024-08-18 22:05:05 +0200

    arguments parsing and application factory for gunicorn

--- autoindex.py
+++ autoindex.py
@@ -1,9 +1,35 @@
-import os.path
+#!/bin/python
+import argparse
+import os
+
 from flask import Flask
 from flask_autoindex import AutoIndex
 
-app = Flask(__name__)
-auto_index = AutoIndex(app, browse_root=os.path.curdir)
+def http_file_server(directory: str | None = None):
+    
+    if directory == None:
+        dir_env = os.getenv("HFS_DIRECTORY")
+        if dir_env != None:
+                directory = dir_env
+        else:
+                directory = os.getcwd()
+
+    app = Flask(__name__)
+    app.config.from_prefixed_env()
+    AutoIndex(app, browse_root=directory)
+    return app
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8888)
+    parser = argparse.ArgumentParser(
+            description='Http Server with flask auto index to serve a list of files')
+    parser.add_argument('-i', '--ip', default='0.0.0.0', type=str,
+            help='the hostname to listen to.')
+    parser.add_argument('-p', '--port', default=8888, type=int,
+            help='the port of the webserver.')
+    parser.add_argument('-d', '--directory', default=None, type=str,
+            help='the directory to serve.')
+
+    args = parser.parse_args()
+    app = http_file_server(args.directory)
+    app.run(host=args.ip, port=args.port)
